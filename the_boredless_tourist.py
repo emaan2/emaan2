ac2f9bd92df7314b48cc4ac0a886cc5cd6be4586
Emaan Hashi <ehashi@unc.edu>
2022-03-14 21:48:50 -0400

    defined function to return the index of a destination

--- the_boredless_tourist.py
+++ the_boredless_tourist.py
@@ -1,4 +1,12 @@
 # recommendation engine for tourists
-
+ 
+ # setting up
 destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+
+# travelling to faraway lands
+def get_destination_index(destination) -> str:
+    destination_index = destinations.index(destination)
+    return destination_index
+
+get_destination_index("Los Angeles, USA")
